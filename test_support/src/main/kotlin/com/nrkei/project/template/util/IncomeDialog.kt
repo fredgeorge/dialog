9829d889f95ff06cfd7607681ba8505d7316e1d7
Fred George <fredgeorge@acm.org>
2026-04-29 16:03:24 -0400

    Fred: Created sample Income Confirmation Dialog

---
+++ test_support/src/main/kotlin/com/nrkei/project/template/util/IncomeDialog.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025-26 by Fred George
+ * @author Fred George  fredgeorge@acm.org
+ * Licensed under the MIT License; see LICENSE file in root.
+ */
+
+package com.nrkei.project.template.util
+
+import com.nrkei.project.dialog.dsl.dialog
+import com.nrkei.project.dialog.model.Acceptable
+import com.nrkei.project.dialog.model.missing
+import com.nrkei.project.dialog.model.problem
+import com.nrkei.project.dialog.questions.IntRangeQuestion
+import com.nrkei.project.dialog.questions.IntRangeQuestion.PositiveIntRange.INVALID
+import com.nrkei.project.dialog.questions.IntRangeQuestion.PositiveIntRange.VALID
+import com.nrkei.project.dialog.questions.YesNoQuestion
+import com.nrkei.project.dialog.questions.YesNoQuestion.YesNoChoice.NO
+import com.nrkei.project.dialog.questions.YesNoQuestion.YesNoChoice.YES
+
+object IncomeDialog {
+    val incomeDialog = dialog {
+        first ask taxIncomeCorrect answers {
+            -YES conclude Acceptable
+            -NO ask correctIncome answers {
+                -INVALID conclude problem("Enter a positive income amount")
+                -VALID ask documentationUploaded answers {
+                    -YES ask beenReviewed answers {
+                        -YES conclude Acceptable
+                        -NO conclude missing("Documentation needs review")
+                    }
+                    -NO conclude missing("Documentation needs to be provided")
+                }
+            }
+        }
+    }
+
+    private val taxIncomeCorrect = YesNoQuestion("Is your tax income correct?")
+    private val correctIncome = IntRangeQuestion.positiveInt("What is the actual income?")
+    private val documentationUploaded = YesNoQuestion("Has income documentation been uploaded?")
+    private val beenReviewed = YesNoQuestion("Has the documentation been reviewed?")
+}
